efa19a783bed471b2cde02b375689883f36e2fcb
chrispyYE <grm.kristjan@outlook.com>
2015-04-09 21:55:51 +0200

    update1
    gogogo

---
+++ sporocanje.php
@@ -0,0 +1,18 @@
+//$_POST["uporid"] ----> dobis id trenutno prijavljenega človeka.<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="sporocanje.css">
+</head>
+
+<body>
+    <?php
+
+?>
+</body>
+</html>
+
+
+
